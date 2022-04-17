ce0a2ddd9a687e5c9273e2018c93cded492dc178
Ishika Agarwal <agarwalishika20@gmail.com>
2022-04-17 18:16:29 +0530

    pages added

--- app/src/main/java/com/example/project_v1/TimeConverterActivity.java
+++ app/src/main/java/com/example/project_v1/TimeConverterActivity.java
@@ -1,4 +1,12 @@
 package com.example.project_v1;
 
-public class TimeConverterActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimeConverterActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_timezones);
+    }
 }
